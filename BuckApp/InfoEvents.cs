d02f80461ff846a9bfbbf8883682a1b7123766d6
alex gonzalez <alex_tamudin@hotmail.com>
2020-05-13 15:01:51 +0200

    restablecer lo de ayer por la noche

--- BuckApp/InfoEvents.cs
+++ BuckApp/InfoEvents.cs
@@ -1,6 +1,4 @@
-﻿using Controller;
-using Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,64 +10,12 @@ using System.Windows.Forms;
 
 namespace BuckApp
 {
-    public partial class combotipo : Form
+    public partial class InfoEvents : Form
     {
         String name, description, localidad, fecha, type;
         int n_participantes, n_maxParticipantes, id_user, userID;
-        EventDAO model_even = new EventDAO();
-        List<Event> events;
-        List<String> typeEvents;
-        private EventController eventController;
-        int comienzo,nummax;
 
-        private void btRegisterEvent_Click(object sender, EventArgs e)
-        {
-            //tbNInscritos.Text = Convert.ToString(n_participantes);
-            //tbMaxParticipantes.Text = Convert.ToString(n_maxParticipantes);
-            //tbTipo.Text = type;
-            comienzo = Convert.ToInt32(tbNInscritos.Text);
-            comienzo = 1;
-            // tbNInscritos.Text = empezar;
-            nummax = Convert.ToInt16(tbMaxParticipantes.Text);
-            DateTime fecha = datepicker.Value;
-            userID = this.user.Id;
-            MessageBox.Show(tbName.Text);
-            MessageBox.Show(tbDescription.Text);
-            MessageBox.Show(tbLocalidad.Text);
-            MessageBox.Show(fecha.ToString());
-
-
-            MessageBox.Show(comienzo.ToString());
-            MessageBox.Show(nummax.ToString());
-            MessageBox.Show(comboBox1.Text);
-            MessageBox.Show(userID.ToString());
-
-
-
-            model_even.insertar_event(tbName.Text, tbDescription.Text, tbLocalidad.Text, fecha.ToString(), comienzo, nummax, comboBox1.Text, userID);
-            tbName.Text = "";
-            tbDescription.Text = "";
-            tbLocalidad.Text = "";
-            tbNInscritos.Text = "";
-            tbMaxParticipantes.Text = "";
-
-            //this.Close();
-        }
-
-        User user;
-
-
-        public combotipo(User user)
-        {
-            InitializeComponent();
-            this.user = user;
-        }
-        private void Registrar_event(object sender, EventArgs e)
-        {
-           
-        }
-
-        public combotipo(String name, String description, String localidad, String fecha, 
+        public InfoEvents(String name, String description, String localidad, String fecha, 
             int n_participantes, int n_maxParticipantes, String type, int id_user, int userID)
         {
             InitializeComponent();
@@ -86,10 +32,8 @@ namespace BuckApp
 
         private void InfoEvents_Load(object sender, EventArgs e)
         {
-            eventController = new EventController();
             ponerDatosTextBox();
             ocultarCampos();
-            cargarComboBox();
         }
         
         private void ponerDatosTextBox()
@@ -102,7 +46,7 @@ namespace BuckApp
             tbMaxParticipantes.Text = Convert.ToString(n_maxParticipantes);
             tbTipo.Text = type;
         }
-        //infoevents
+
         private void ocultarCampos()
         {
             if (id_user != userID)
@@ -116,15 +60,6 @@ namespace BuckApp
                 tbTipo.ReadOnly = true;
                 btModify.Enabled = false;
                 btDelete.Enabled = false;
-            }
-        }
-
-        private void cargarComboBox()
-        {
-            typeEvents = eventController.cargarDatosComboBox();
-            for (int i = 0; i < typeEvents.Count; i++)
-            {
-                comboBox1.Items.Add(typeEvents[i]);
             }
         }
     }
