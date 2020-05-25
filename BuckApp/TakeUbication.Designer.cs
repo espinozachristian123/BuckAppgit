285ecc10c7e1a62a723e9bf833a3634f494b4a3b
christian espinoza <espinozachristian1997@gmail.com>
2020-05-26 00:22:44 +0200

    Puesto ICONO en algunas ventanas

--- BuckApp/TakeUbication.Designer.cs
+++ BuckApp/TakeUbication.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TakeUbication));
             this.wbMaps = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
@@ -46,6 +47,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.wbMaps);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "TakeUbication";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TakeUbication";
